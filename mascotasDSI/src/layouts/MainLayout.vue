8f58198474ffa6d1e98fb620f43703971321ffe2
FranLinares06 <lb21001@ues.edu.sv>
2025-03-19 11:09:33 -0600

    Definicion de paleta de colores
    Estableci la paleta de colores acordada como grupo en el archivo quasar.variables.scss, además agregue route al btn inicio de sesion y a inicio

--- mascotasDSI/src/layouts/MainLayout.vue
+++ mascotasDSI/src/layouts/MainLayout.vue
@@ -1,25 +1,24 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="header">
       <q-toolbar>
         <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" size="xl"/> -->
 
         <q-toolbar-title>
           <q-avatar size="50px">
-            <img src="../assets/confirmado.png">
+            <img src="../assets/confirmado.png" />
           </q-avatar>
           <span class="q-ml-sm text-h5">Adoptame SV</span>
         </q-toolbar-title>
         <q-btn-group flat>
-          <q-btn flat label="Inicio"/>
+          <q-btn flat label="Inicio" to="/" />
           <q-btn flat label="Quienes somos" />
           <q-btn flat label="catalogo de adopcion" />
           <q-btn flat label="dar en adopcion" />
           <q-btn flat label="politicas" />
           <q-btn flat label="contactanos" />
         </q-btn-group>
-        <q-btn rounded unelevated color="black" label="Iniciar Sesión" />
+        <q-btn rounded unelevated color="black" label="Iniciar Sesión" to="/inicio-sesion" />
       </q-toolbar>
     </q-header>
 
@@ -32,35 +31,30 @@
     </q-page-container>
 
     <q-footer elevated class="footer text-white q-pa-md absolute-bottom">
-  <q-toolbar class="row justify-between items-center q-gutter-md">
+      <q-toolbar class="row justify-between items-center q-gutter-md">
+        <!-- Logo -->
+        <div class="col text-center q-mt-lg">
+          <img height="100px" src="../assets/confirmado.png" />
+        </div>
 
-    <!-- Logo -->
-    <div class="col text-center q-mt-lg">
-      <img height="100px" src="../assets/confirmado.png">
-    </div>
+        <!-- Contactos -->
+        <div class="col text-center">
+          <div class="text-h6">Contactos</div>
+          <div class="q-mt-xs text-body1">+503 7589-8455</div>
+          <div class="q-mt-xs text-body1">adoptamesv@gmail.com</div>
+        </div>
 
-    <!-- Contactos -->
-    <div class="col text-center">
-      <div class="text-h6">Contactos</div>
-      <div class="q-mt-xs text-body1">+503 7589-8455</div>
-      <div class="q-mt-xs text-body1">adoptamesv@gmail.com</div>
-    </div>
-
-    <!-- Redes -->
-    <div class="col flex flex-center column">
-      <div class="row q-gutter-sm">
-        <q-btn round color="white" icon="fab fa-tiktok" text-color="black" />
-        <q-btn round color="white" icon="fab fa-facebook-f" text-color="black" />
-        <q-btn round color="white" icon="fab fa-whatsapp" text-color="black" />
-      </div>
-      <div class="q-mt-sm text-center text-body1">Redes</div>
-    </div>
-
-  </q-toolbar>
-</q-footer>
-
-
-
+        <!-- Redes -->
+        <div class="col flex flex-center column">
+          <div class="row q-gutter-sm">
+            <q-btn round color="white" icon="fab fa-tiktok" text-color="black" />
+            <q-btn round color="white" icon="fab fa-facebook-f" text-color="black" />
+            <q-btn round color="white" icon="fab fa-whatsapp" text-color="black" />
+          </div>
+          <div class="q-mt-sm text-center text-body1">Redes</div>
+        </div>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -75,13 +69,12 @@
 </script>
 
 <style scoped>
-  .header{
-    background-color: #FCCA73;
-    color: #3C261B;
-  }
-  .footer{
-    background-color: #8C3223;
-    color: white;
-  }
-
+.header {
+  background-color: #fcca73;
+  color: #3c261b;
+}
+.footer {
+  background-color: #8c3223;
+  color: white;
+}
 </style>
--- mascotasDSI/src/router/routes.js
+++ mascotasDSI/src/router/routes.js
@@ -3,16 +3,17 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'inicio-sesion', component: () => import('pages/InicioSesion.vue') }, //Ruta para pagina de inicio de sesion
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
