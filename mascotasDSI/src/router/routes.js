f9439503df81177e6e9f3c41b472a6a72403c584
alexooscar02 <gs21009@ues.edu.sv>
2025-03-20 13:41:23 -0600

    Modifician en el router, se le aplico un layout disitinto a la pagina de inicio de sesion

--- mascotasDSI/src/router/routes.js
+++ mascotasDSI/src/router/routes.js
@@ -4,18 +4,23 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'listado', component: () => import('pages/ListadoPage.vue') }, //Ruta para catalogo
-      { path: 'inicio-sesion', component: () => import('pages/InicioSesion.vue') }, //Ruta para pagina de inicio de sesion
-      { path: 'inicio-sesion/crear-cuenta', component: () => import('pages/Crearcuenta.vue') }, //Ruta para crear cuenta
-      { path: 'agregar', component: () => import('pages/AgregarPage.vue') }, //Ruta para agregar mascota
-      { path: 'quienessomos', component: () => import('pages/QuienesSomos.vue') }, //quienes somos
-      { path: 'detalle/:IDMASCOTA', component: () => import('pages/DetalleMascota.vue') }, //detalle mascota
-      { path: 'miperfil', component: () => import('pages/MiPerfil.vue') }, //Mi perfil
-      { path: 'tutorial', component: () => import('pages/MiTutorial.vue') }, //Tutorial
-      { path: 'sugerencias', component: () => import('pages/MiSugerencias.vue') }, //Tutorial
+      { path: 'listado', component: () => import('pages/ListadoPage.vue') },
+      { path: 'inicio-sesion', component: () => import('pages/InicioSesion.vue') },
+      { path: 'agregar', component: () => import('pages/AgregarPage.vue') },
+      { path: 'quienessomos', component: () => import('pages/QuienesSomos.vue') },
+      { path: 'detalle/:IDMASCOTA', component: () => import('pages/DetalleMascota.vue') },
+      { path: 'miperfil', component: () => import('pages/MiPerfil.vue') },
+      { path: 'tutorial', component: () => import('pages/MiTutorial.vue') },
+      { path: 'sugerencias', component: () => import('pages/MiSugerencias.vue') },
     ],
   },
-
+  {
+    path: '/inicio-sesion/crear-cuenta',
+    component: () => import('layouts/InicioLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/CrearCuenta.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
